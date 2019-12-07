d1c52fde0de5d7e3ae3ea02301574c8aa209d535
Kris Jurgowski <kris.jurgowski@gmail.com>
2019-12-06 23:52:52 -0500

    cleaner

--- 2019/Advent2019/Advent2019/Day6.swift
+++ 2019/Advent2019/Advent2019/Day6.swift
@@ -8,7 +8,7 @@ func day6a(_ input: String) -> Int {
         .components(separatedBy: CharacterSet.newlines)
         .map { $0.components(separatedBy: ")") }
 
-    let map = orbits.reduce(into: [:]) { $0[$1[1]] = $1[0]}
+    let map = orbits.reduce(into: [:]) { $0[$1[1]] = $1[0] }
     return map.keys.reduce(0) { $0 + Array(sequence(first: $1) { map[$0] }).count - 1 }
 }
 
@@ -17,9 +17,9 @@ func day6b(_ input: String) -> Int {
         .components(separatedBy: CharacterSet.newlines)
         .map { $0.components(separatedBy: ")") }
 
-    let map = orbits.reduce(into: [:]) { $0[$1[1]] = $1[0]}
-    let path: [String] = Array(sequence(first: "YOU") { map[$0] }).reversed()
-    let sPath: [String] = Array(sequence(first: "SAN") { map[$0] }).reversed()
+    let map = orbits.reduce(into: [:]) { $0[$1[1]] = $1[0] }
+    let path:  [String] = sequence(first: "YOU") { map[$0] }.reversed()
+    let sPath: [String] = sequence(first: "SAN") { map[$0] }.reversed()
     let matchCount = zip(path, sPath).reduce(0) { $0 + ($1.0 == $1.1 ? 1 : 0) } + 1
 
     return path.count + sPath.count - (matchCount * 2)
